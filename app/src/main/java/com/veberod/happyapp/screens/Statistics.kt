7a107d18f3574efb35703cae444a76b8802f91ce
dtrix90 <dominique_90_@hotmail.com>
2023-01-04 11:31:39 +0100

    graph
    need x and y values, connect to db

--- app/src/main/java/com/veberod/happyapp/screens/Statistics.kt
+++ app/src/main/java/com/veberod/happyapp/screens/Statistics.kt
@@ -1,12 +1,357 @@
 package com.veberod.happyapp.screens
 
+import android.graphics.Paint
+import android.graphics.PointF
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 
-/*TODO
-*   Show statistics from your own usage of the app, gather data from DB for: Today, yesterday,
-*   this week, month and year*/
 @Composable
 fun Statistics() {
+    val yStep = 50
+    val random = Random
+    /* to test with random points */
+    //TODO enter datapoints from database here
+    val points = (0..9).map {
+        var num = random.nextInt(350)
+        if (num <= 50)
+            num += 100
+        num.toFloat()
+    }
+    // test with fixed points
+    //val points = listOf(150f,100f,250f,200f,330f,300f,90f,120f,285f,199f)
+    Column(modifier = Modifier.fillMaxSize()) {
 
+        TextW()
+        Graph(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp),
+            xValues = (0..9).map { it + 1 },
+            yValues = (0..6).map { (it + 1) * yStep },
+            points = points,
+            paddingSpace = 16.dp,
+            verticalStep = yStep
+        )
+        ButtonMenu()
+    }
+}
+
+@Composable
+fun TextW() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+        Text(
+            text = "Happiness Graph",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(20.dp),
+            fontSize = 30.sp
+        )
+    }
+}
+
+@Composable
+fun ButtonMenu() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(2.dp)
+    ) {
+        Button( onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(8.dp)
+            //elevation = ButtonDefaults.elevation(defaultElevation = 50.dp, pressedElevation = 8.dp)
+            ){
+            Text(
+                text = "Today",
+                style = TextStyle(fontSize = 15.sp)
+            )
+        }
+        Button( onClick = { /*TODO*/ }) {
+            Text(
+                text = "Yesterday",
+                style = TextStyle(fontSize = 15.sp)
+            )
+        }
+        Button( onClick = { /*TODO*/ }) {
+            Text(
+                text = "Week",
+                style = TextStyle(fontSize = 15.sp)
+            )
+        }
+        Button( onClick = { /*TODO*/ }) {
+            Text(
+                text = "6 months",
+                style = TextStyle(fontSize = 15.sp)
+            )
+        }
+    }
+}
+
+/*
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun Menu() {
+    val contextForToast = LocalContext.current.applicationContext
+
+    val listItems = arrayOf("Today", "Yesterday", "Week", "6 months")
+
+    var selectedItem by remember {
+        mutableStateOf(listItems[0])
+    }
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    // the box
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()) {
+
+
+            // text field
+            TextField(
+                value = selectedItem,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(text = "Period") },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(
+                        expanded = expanded
+                    )
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+
+                )
+        }
+
+        // menu
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            listItems.forEach { selectedOption ->
+                // menu item
+                DropdownMenuItem(onClick = {
+                    selectedItem = selectedOption
+                    Toast.makeText(contextForToast, selectedOption, Toast.LENGTH_SHORT).show()
+                    expanded = false
+                }) {
+                    Text(text = selectedOption)
+                }
+            }
+        }
+    }
+
+}*/
+
+/*
+@Composable
+fun Menu() {
+    var mExpanded by remember { mutableStateOf(false) }
+
+    // Create a list of cities
+    val mPeriod = listOf("Today", "Yesterday", "Week", "Month", "6 months", "Year")
+
+    // Create a string value to store the selected city
+    var mSelectedText by remember { mutableStateOf("") }
+
+    var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    // Up Icon when expanded and down icon when collapsed
+    val icon = if (mExpanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column(Modifier.padding(20.dp)) {
+
+        // Create an Outlined Text Field
+        // with icon and not expanded
+        OutlinedTextField(
+            value = mSelectedText,
+            onValueChange = { mSelectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    // This value is used to assign to
+                    // the DropDown the same width
+                    mTextFieldSize = coordinates.size.toSize()
+                },
+            label = {Text("Label")},
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { mExpanded = !mExpanded })
+            }
+        )
+
+        // Create a drop-down menu with list of cities,
+        // when clicked, set the Text Field text as the city selected
+        DropdownMenu(
+            expanded = mExpanded,
+            onDismissRequest = { mExpanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){mTextFieldSize.width.toDp()})
+        ) {
+            mPeriod.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    mSelectedText = label
+                    mExpanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+    }
+}
+*/
+
+@Composable
+fun Graph(
+    modifier: Modifier,
+    xValues: List<Int>,
+    yValues: List<Int>,
+    points: List<Float>,
+    paddingSpace: Dp,
+    verticalStep: Int
+) {
+    val controlPoints1 = mutableListOf<PointF>()
+    val controlPoints2 = mutableListOf<PointF>()
+    val coordinates = mutableListOf<PointF>()
+    val density = LocalDensity.current
+    val textPaint = remember(density) {
+        Paint().apply {
+            color = android.graphics.Color.BLACK
+            textAlign = Paint.Align.CENTER
+            textSize = density.run { 12.sp.toPx() }
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .background(Color.White)
+            .padding(horizontal = 8.dp, vertical = 12.dp),
+        contentAlignment = Center
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
+            val yAxisSpace = size.height / yValues.size
+            /** placing x axis points */
+            for (i in xValues.indices) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    "${xValues[i]}",
+                    xAxisSpace * (i + 1),
+                    size.height - 30,
+                    textPaint
+                )
+            }
+            /** placing y axis points */
+            for (i in yValues.indices) {
+                drawContext.canvas.nativeCanvas.drawText(
+                    "${yValues[i]}",
+                    paddingSpace.toPx() / 2f,
+                    size.height - yAxisSpace * (i + 1),
+                    textPaint
+                )
+            }
+            /** placing our x axis points */
+            for (i in points.indices) {
+                val x1 = xAxisSpace * xValues[i]
+                val y1 = size.height - (yAxisSpace * (points[i] / verticalStep.toFloat()))
+                coordinates.add(PointF(x1, y1))
+                /** drawing circles to indicate all the points */
+                drawCircle(
+                    color = Color.Red,
+                    radius = 10f,
+                    center = Offset(x1, y1)
+                )
+            }
+            /** calculating the connection points */
+            for (i in 1 until coordinates.size) {
+                controlPoints1.add(
+                    PointF(
+                        (coordinates[i].x + coordinates[i - 1].x) / 2,
+                        coordinates[i - 1].y
+                    )
+                )
+                controlPoints2.add(
+                    PointF(
+                        (coordinates[i].x + coordinates[i - 1].x) / 2,
+                        coordinates[i].y
+                    )
+                )
+            }
+            /** drawing the path */
+            val stroke = Path().apply {
+                reset()
+                moveTo(coordinates.first().x, coordinates.first().y)
+                for (i in 0 until coordinates.size - 1) {
+                    cubicTo(
+                        controlPoints1[i].x, controlPoints1[i].y,
+                        controlPoints2[i].x, controlPoints2[i].y,
+                        coordinates[i + 1].x, coordinates[i + 1].y
+                    )
+                }
+            }
+
+            /** filling the area under the path */
+            val fillPath = android.graphics.Path(stroke.asAndroidPath())
+                .asComposePath()
+                .apply {
+                    lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
+                    lineTo(xAxisSpace, size.height - yAxisSpace)
+                    close()
+                }
+            drawPath(
+                fillPath,
+                brush = Brush.verticalGradient(
+                    listOf(
+                        Color.Cyan,
+                        Color.Transparent,
+                    ),
+                    endY = size.height - yAxisSpace
+                ),
+            )
+            drawPath(
+                stroke,
+                color = Color.Black,
+                style = Stroke(
+                    width = 5f,
+                    cap = StrokeCap.Round
+                )
+            )
+        }
+    }
 }
