8a390781a75082fb62f5eecd5c83bdf9f8d47524
Hampus Andersson <70724079+Hampster8@users.noreply.github.com>
2023-01-09 18:47:59 +0100

    map

--- app/src/main/java/com/veberod/happyapp/map_feature/presentation/components/Map.kt
+++ app/src/main/java/com/veberod/happyapp/map_feature/presentation/components/Map.kt
@@ -1,5 +1,6 @@
 package com.veberod.happyapp.map_feature.presentation.components
 
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -7,55 +8,37 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 import com.google.maps.android.heatmaps.HeatmapTileProvider
-import com.veberod.happyapp.database.domain.model.LatLngConverter
-import com.veberod.happyapp.database.domain.repository.MoodRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-
 @Composable
-fun Map(moodRepository: MoodRepository) {
+fun Map() {
     val veberod = LatLng(55.6364, 13.5006)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(veberod, 15f)
     }
-    val heatMapPoints: MutableList<LatLng> = mutableListOf()
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
         Marker(
             state = MarkerState(position = veberod),
-            title = "Veberöd"
-
+            title = "Veberöd",
+            //snippet = ""
         )
-        val scope = CoroutineScope(Dispatchers.Default)
-        scope.launch {
-            withContext(Dispatchers.IO) {
-                val moods = moodRepository.getAllMoods()
-                val latLngConverter = LatLngConverter()
-                for (mood in moods) {
-                    val latLng = latLngConverter.toLatLng(mood.geolocation)
-                    // Add the LatLng point to the heatMapPoints list
-                    heatMapPoints.add(latLng)
-                }
-            }
-        }
-
+        val heatMapPoints: List<LatLng> = listOf(
+            LatLng(55.6350, 13.4958),
+            LatLng(55.6355, 13.4955),
+            LatLng(55.6360, 13.4968),
+            LatLng(55.6365, 13.4965),
+            LatLng(55.6365, 13.4965),
+            LatLng(55.6365, 13.4965),
+            LatLng(55.6365, 13.4965),
+            LatLng(55.6365, 13.4965),
+            LatLng(55.6365, 13.4965))
+        // Create a HeatmapTileProvider object using the specified list of LatLng points
         val heatmapTileProvider = HeatmapTileProvider.Builder()
             .data(heatMapPoints)
             .opacity(0.5)
             .build()
-        // Create a HeatmapTileProvider object using the heatMapPoints list as the data
-
         // Add a TileOverlay to the map using the HeatmapTileProvider object as the tile provider
         TileOverlay(tileProvider = heatmapTileProvider)
-
     }
-
 }
-
-
-
-
